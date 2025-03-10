13711e7e5e956854491d5c95029a484ed9a3b860
TTG-0513 <viper1305@freenet.de>
2025-03-10 16:58:43 +0100

    cleaning

--- wetter_check_2025/lib/wetter_check.dart
+++ wetter_check_2025/lib/wetter_check.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:wetter_check_2025/backgrund_wetter_app.dart';
 
 class WetterCheck extends StatefulWidget {
   const WetterCheck({super.key});
